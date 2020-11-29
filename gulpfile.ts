043b157ba14074fecce01693d97da52ce969630c
Vikash Kothary <kothary.vikash@gmail.com>
2020-11-29 17:08:44 +0000

    Run storybook commands as a child process using gulp

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,6 +5,7 @@
 import * as gulp from 'gulp';
 import * as debug from 'gulp-debug';
 import * as exec from 'gulp-exec';
+import { exec as sh } from 'child_process';
 import * as eslint from 'gulp-eslint';
 
 
@@ -17,11 +18,42 @@ const config = {
 	typescript: {
 		configPath: 'packages/internals/src/lint/.eslintrc',
 		ignorePath: 'packages/internals/src/lint/.eslintignore'
+	},
+	storybook: {
+		port: 31000,
+		configFolder: 'packages/internals/src/storybook',
+		buildFolder: 'build/docs'
 	}
 }
 
 
 // Internals
+const buildStorybook = (cb) => {
+	let cmd = `yarn build-storybook`;
+	cmd += ` -c ${config.storybook.configFolder}`;
+	cmd += ` -o ${config.storybook.buildFolder}`;
+	// cmd += ` --docs`;
+	// cmd += ` --watch`;
+	cmd += ` --quiet`;
+	sh(cmd, (err, stdout, stderr) => {
+		console.log(stdout);
+		console.log(stderr);
+		cb(error);
+	});
+}
+
+const runStorybook = (cb) => {
+	let cmd = `yarn start-storybook`;
+	cmd += ` -c ${config.storybook.configFolder}`;
+	cmd += ` -p ${config.storybook.port}`;
+	cmd += ` --quiet`;
+	sh(cmd, (err, stdout, stderr) => {
+		console.log(stdout);
+		console.log(stderr);
+		cb(error);
+	});
+}
+
 const placeholder = () => {
 	return gulp.src(config.src.typescript)
 		.pipe(debug())
@@ -55,7 +87,8 @@ gulp.task('postinstall:deduplicate', deduplicate);
 gulp.task('postinstall:autoclean', autoclean);
 gulp.task('lint:typescript', lintTS);
 gulp.task('lint:typescript:fix', placeholder);
-
+gulp.task('build:storybook', buildStorybook)
+gulp.task('run:storybook', runStorybook)
 
 // Public
 gulp.task('default', placeholder);
@@ -63,11 +96,15 @@ gulp.task('postinstall', gulp.series(
 	'postinstall:deduplicate',
 	'postinstall:autoclean'
 ));
-gulp.task('docs', placeholder);
+gulp.task('docs', gulp.parallel(
+	'run:storybook'
+));
 gulp.task('lint', gulp.series(
 	'lint:typescript',
 	'lint:typescript:fix'
 ));
 gulp.task('test', placeholder);
-gulp.task('build', placeholder);
+gulp.task('build', gulp.parallel(
+	'build:storybook'
+));
 gulp.task('start', placeholder);
