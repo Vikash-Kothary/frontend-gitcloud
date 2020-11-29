e3cb4cfa794b174ae369da3a129276e7f48eccb9
Vikash Kothary <kothary.vikash@gmail.com>
2020-11-29 18:57:29 +0000

    Add script to download project dependencies

---
+++ scripts/init-frontend.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# file: init-frontend.sh
+
+# Ensure it's in the correct directory
+# Needed for multi module projects
+pushd "${GITCLOUD_FRONTEND_PATH}" 
+
+init_default() {
+	echo "[Node]: $(node -v)"
+	echo "[NPM] : $(npm -v)"
+	echo "[Yarn]: $(yarn -v)"
+	echo
+	${YARN} install
+}
+
+case "${ENV}" in
+	"local" ) init_default ;;
+	* ) init_default ;;
+esac 
+
+popd
