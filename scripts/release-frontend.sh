ab3d1400f94868f70eaf98b5eae32aa50cb0ecbc
Vikash Kothary <kothary.vikash@gmail.com>
2021-02-05 15:33:26 +0000

    Add release script to CI pipeline

---
+++ scripts/release-frontend.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# file: release-frontend.sh
+# description: Create new project version release.
+
+RELEASE_VERSION=${GITCLOUD_FRONTEND_VERSION//\"}-${GIT_COMMIT_SHORT_SHA//\"}
+
+# If is a stable release
+if [[ "\"main\"" == "${GIT_BRANCH}" ]]; then
+	RELEASE_VERSION=${GITCLOUD_FRONTEND_VERSION//\"}
+fi
+
+echo "[RELEASE] New release version: ${RELEASE_VERSION}"
+
+${YARN} lerna version ${RELEASE_VERSION} --yes
